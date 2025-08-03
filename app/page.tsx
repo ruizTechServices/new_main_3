a3f2ab3864d5db71cd9c069c98d8d4b19214cf64
Giovanni <ruiztechservices@gmail.com>
2025-08-02 22:38:08 -0400

    feat: add error parsing utility and animated ball canvas component with tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-
 export default function Home() {
   return (
-    <div className="w-screen h-screen bg-cyan-200">
+    <div className="w-screen h-screen bg-cyan-200 overflow-hidden">
     </div>
-  );
+  )
 }
